69b684f5419224cf4bd6a37de513975ac755cd74
anshumansinha18 <sinhaanshuman18298@gmail.com>
2023-02-26 02:49:17 +0530

    Sending JSON as response from server

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
+// const http = require("http");
+// const PORT = 8081;
+
+// const server = http
+//   .createServer((request, response) => {
+//     //THIS CALLABCK WILL ONLY GET EXECUTED WHEN THERE IS REQUEST TO THE SERVER.
+//     const date = new Date();
+//     const day = date.toLocaleDateString();
+//     const time = date.toLocaleTimeString();
+//     //RESPONSE.WRITE takes parameter as a string. Whatever we write in the parameter, is passed
+//     // as a response to the client for the request it made.
+//     response.write(`<h1>${day} ${time}</h1>`);
+//     response.end(); //If it is not here, then the request will keep waiting for the response.
+//   })
+//   .listen(PORT, () => {
+//     //THIS CALLBACK WILL EXECUTE AS SOON AS THE SERVER STARTS
+//     console.log("Listening on PORT:", PORT);
+//   });
+
+//SENDING JSON AS RESPONSE
 const http = require("http");
 const PORT = 8081;
 
-const server = http
+http
   .createServer((request, response) => {
-    //THIS CALLABCK WILL ONLY GET EXECUTED WHEN THERE IS REQUEST TO THE SERVER.
-    const date = new Date();
-    const day = date.toLocaleDateString();
-    const time = date.toLocaleTimeString();
-    //RESPONSE.WRITE takes parameter as a string. Whatever we write in the parameter, is passed
-    // as a response to the client for the request it made.
-    response.write(`<h1>${day} ${time}</h1>`);
-    response.end(); //If it is not here, then the request will keep waiting for the response.
+    const serverInfo = {
+      serverName: "Crio Server",
+      version: "1.0.0",
+      currentDate: new Date().toLocaleDateString(),
+      currentTime: new Date().toLocaleTimeString(),
+    };
+    response.write(JSON.stringify(serverInfo));
+    response.end();
   })
   .listen(PORT, () => {
-    //THIS CALLBACK WILL EXECUTE AS SOON AS THE SERVER STARTS
-    console.log("Listening on PORT:", PORT);
+    console.log("Listening on PORT", PORT);
   });
