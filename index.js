1cb0cc03a071f71bcd41807be77320955cc47df7
anshumansinha18 <sinhaanshuman18298@gmail.com>
2023-02-26 02:40:25 +0530

    response and request to the server

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const server = http
   .createServer((request, response) => {
     //THIS CALLABCK WILL ONLY GET EXECUTED WHEN THERE IS REQUEST TO THE SERVER.
     const date = new Date();
-    console.log(date.toLocaleDateString());
-    console.log(date.toLocaleTimeString());
+    const day = date.toLocaleDateString();
+    const time = date.toLocaleTimeString();
+    //RESPONSE.WRITE takes parameter as a string. Whatever we write in the parameter, is passed
+    // as a response to the client for the request it made.
+    response.write(`<h1>${day} ${time}</h1>`);
     response.end(); //If it is not here, then the request will keep waiting for the response.
   })
   .listen(PORT, () => {
