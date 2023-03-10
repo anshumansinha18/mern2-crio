ac1d75e37c0d6a65e8d82eabea77105694a2376a
anshumansinha18 <sinhaanshuman18298@gmail.com>
2023-03-11 00:29:28 +0530

    Validating Emails using Regex for schema

--- models/blogs.model.js
+++ models/blogs.model.js
@@ -4,7 +4,7 @@ const authorSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      maxLenght: 25,
+      maxLength: 25,
     },
     twitterHandle: {
       type: String,
@@ -12,7 +12,19 @@ const authorSchema = mongoose.Schema(
     email: {
       type: String,
       required: true,
-      maxLenght: 50,
+      maxLength: 50,
+      validate: (newValue) => {
+        //the name of the function should always be validate
+        //Logic for validation
+        //function should return true or false
+        const re =
+          /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (re.test(newValue)) {
+          return true;
+        } else {
+          return false;
+        }
+      },
     },
     image: {
       type: String,
